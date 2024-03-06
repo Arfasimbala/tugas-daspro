9d391d7b25fddcf93cd97269e3a93cb3a2e338c7
Arfasimbala <162411059+Arfasimbala@users.noreply.github.com>
2024-03-06 14:21:05 +0800

    Update arfa_simbala.py

--- arfa_simbala.py
+++ arfa_simbala.py
@@ -1,8 +1,12 @@
-hari_kerja = 30
+hari_kerja = 31
 hari_kerja_perbulan = 30
 gaji = 80000000
+gaji_lembur = 100000
 
-Total_Gaji = (hari_kerja/hari_kerja_perbulan) *gaji
-Total_Gaji = int (Total_Gaji)
-Penghasilan = "{:,.2f}" .format(Total_Gaji)
+if hari_kerja > hari_kerja_perbulan:
+    Total_gaji = (hari_kerja/hari_kerja_perbulan)* gaji + gaji_lembur
+elif hari_kerja < hari_kerja_perbulan:
+    Total_gaji = (hari_kerja/hari_kerja_perbulan)* gaji
+
+Penghasilan = "{:,.2f}" .format( Total_gaji )
 print (Penghasilan)
